36c921f54fdc833ad6fb2ea5b010592171f775db
keyready <vip.korchak@list.ru>
2023-10-25 15:30:44 +0300

    отображение голых данных

--- server/index.js
+++ server/index.js
@@ -1,55 +1,14 @@
-const XLSX = require('xlsx');
+const express = require('express')
+const { getRectangleFromExcel } = require("./parser");
+const app = express()
+const cors = require('cors')
 
-function getRectangleFromExcel(fileName, rectangleVertices) {
-    const workbook = XLSX.readFile(fileName);
+app.use(express.json())
+app.use(cors())
 
-    const sheet_name_list = workbook.SheetNames;
-    const worksheet = workbook.Sheets[sheet_name_list[0]];
+app.get('/getSchedule', (req, res) => {
+    const schedule = getRectangleFromExcel('../files/611-2.xlsx', 'D6:Z34')
+    return res.status(200).json(schedule)
+})
 
-    const vertices = rectangleVertices.split(':').map(function (vertex) {
-        return XLSX.utils.decode_cell(vertex);
-    });
-
-    const data = XLSX.utils.sheet_to_json(worksheet, {header: 1});
-
-    const selectedData = [];
-
-    for (let col = vertices[0].c; col <= vertices[1].c; col++) {
-        const columnData = [];
-        for (let row = vertices[0].r; row <= vertices[1].r; row++) {
-            columnData.push(data[row][col]);
-        }
-        selectedData.push(columnData);
-    }
-
-    const str = [];
-
-    selectedData.map((column, index) => {
-        str.push([]);
-        column.map((cell) => {
-            if (/^\d+$/.test(cell)) {
-                str[index].push('             ' + new Date(
-                    (cell - (25567 + 2)) * 86400 * 1000
-                ).toLocaleString('ru-RU', {
-                    day: '2-digit',
-                    month: '2-digit',
-                    year: '2-digit'
-                }) + '             ')
-            } else if (!cell?.length) {
-                str[index].push(`Тип занятия: самоподготовка, аудитория: 314-6`)
-            } else if (cell.includes('\r\n')) {
-                const row = cell.split('\r\n');
-                str[index].push(
-                    `Тип занятия: ${row[0]}, дисциплина: ${row[1]}, аудитория: ${row[2]}`
-                );
-            } else if (/[А-ЯЁёа-я]/.test(cell)) {
-                str[index].push('                ' + cell + '                ');
-            }
-        });
-    });
-
-    return str;
-}
-
-
-console.log(getRectangleFromExcel('../files/611-2.xlsx', 'D6:Z34'));
+app.listen(5000)
